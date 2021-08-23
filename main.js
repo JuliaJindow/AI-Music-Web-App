aabff7171cd916128aa75918a48e11f1e5fab4bd
Julia <74714789+JuliaJindow@users.noreply.github.com>
2021-08-23 19:55:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var song1 = "";
+var song2 = "";
+leftX = 0;
+leftY = 0;
+rightX = 0;
+rightY = 0;
+
+function preload() {
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video,0,0,600,500);
+}
+
+function modelLoaded() {
+    console.log("The model is loaded!");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+     console.log(results);
+     
+     console.log("Score of left hand wrist is = " + scoreLeft);
+     leftWristX = results[0].pose.leftWrist.x;
+     leftWristY = results[0].pose.leftWrist.y;
+     console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+     rightWristX = results[0].pose.rightWrist.x;
+     rightWristY = results[0].pose.rightWrist.y;
+     console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
